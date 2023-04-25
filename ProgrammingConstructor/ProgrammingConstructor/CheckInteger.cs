816828bfc490810d55cb40f1849bb829d6463234
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-25 14:38:13 +0530

    Add : checkig the integer is equal or not

---
+++ ProgrammingConstructor/ProgrammingConstructor/CheckInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructor
+{
+    public class CheckInteger
+    {
+        int num_One;
+        int num_Two;
+        public void ChecktheNum()
+           
+        {
+            Console.WriteLine("Enter the Number:");
+            num_One=Convert.ToInt32(Console.ReadLine());
+            num_Two=Convert.ToInt32(Console.ReadLine());
+
+            if(num_One == num_Two)
+            {
+                Console.WriteLine("Number is equal.");
+            }
+            else
+            {
+                 Console.WriteLine("Number is not equal");
+            }
+        }
+    }
+}
---
+++ ProgrammingConstructor/ProgrammingConstructor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingConstructor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckInteger num=new CheckInteger();
+            num.ChecktheNum();
+        }
+    }
+}
